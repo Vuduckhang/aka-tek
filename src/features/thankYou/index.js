ecf69e25fe8714e394b2e74a2f285389586822ea
khang <vuduckhang99@gmail.com>
2023-05-09 11:52:05 +0700

    Create ui thank you

--- src/features/thankYou/index.js
+++ src/features/thankYou/index.js
@@ -1,20 +1,41 @@
 import React from 'react'
 import Footer from '../../components/Footer'
 import Header from '../../components/Header'
-import ItemCard from '../../components/ItemCard'
-import BillingDetail from '../checkOut/BillingDetail'
-import Payment from '../checkOut/Payment'
+import { Button } from 'antd'
+import { useNavigate } from 'react-router'
+
+const ThankYouIcon = require('../../img/Group.jpg')
 
 const ThankYou = () => {
+  const navigate = useNavigate()
   return (
     <div className='flex'>
       <Header />
-      <ItemCard title={'Check out'}>
-        <div class=''>
-          <BillingDetail />
-          <Payment />
+      <div class='thank-you d-flex justify-content-center'>
+        <div class='text-center'>
+          <div class='d-flex justify-content-center'>
+            <div class='thank-you__icon'>
+              <img src={ThankYouIcon} />
+            </div>
+          </div>
+          <div class='thank-you__title gray-1-color my-2'>
+            Thank you for your purchase!
+          </div>
+          <div class='thank-you__text'>Your order number is #STE1587975295</div>
+          <div class='thank-you__text'>
+            We will email you an order confirmation with details.
+          </div>
+          <Button
+            className='order-details__button thank-you__button mt-5'
+            type='primary'
+            onClick={() => {
+              navigate('/')
+            }}
+          >
+            Back to Homepage
+          </Button>
         </div>
-      </ItemCard>
+      </div>
       <Footer />
     </div>
   )
